322b8f122624e995aac85a236656475cac13c686
Ryan Tanay <ryantanay@newevolution.org>
2015-07-17 10:16:17 -0500

    Copied code from codepen locally & modified for non-codepen display.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # funcception
+
+  ...........YOUR MISSION...........
+
+  Make me a sandwich.
+  
+  1. Create a Sandwich object.
+  2. Create 6 keys on the object all defaulted to a value of false
+     a. pickle: false
+     b. lettuce: false
+     c. tomato: false
+     d. ketchup: false
+     e. mustard: false
+     f. mayo: false
+  3. Create different functions where each one can change the value of only one of the keys on a Sandwich object to "true".
+  4. Create a function named "makeMeASandwich".
+  5. That function should take 6 arguments - one for each condiment on the sandwich.
+  6. If any of those 6 argument values are true, put that condiment on the sandwich.
+  7. Write to the document something like this...
+    "My sandwich has pickle, ketchup, mustard on it"
---
+++ sammich.js
@@ -0,0 +1,70 @@
+var docOutput = [];
+var condimentCount = 0;
+
+var sandwich = {
+  pickle: false,
+  lettuce: false,
+  tomato: false,
+  ketchup: false,
+  mustard: false,
+  mayo: false
+}
+
+function pickler(value) {
+  sandwich.pickle = value;
+}
+
+function lettucer(value) {
+  sandwich.lettuce = value;
+}
+
+function tomatoer(value) {
+  sandwich.tomato = value;
+}
+
+function ketchuper(value) {
+  sandwich.ketchup = value;
+}
+
+function mustarder(value) {
+  sandwich.mustard = value;
+}
+
+function mayoer(value) {
+  sandwich.mayo = value;
+}
+
+function makeMeASandwich(pickle, lettuce, tomato, ketchup, mustard, mayo) {
+  pickler(pickle);
+  lettucer(lettuce);
+  tomatoer(tomato);
+  ketchuper(ketchup);
+  mustarder(mustard);
+  mayoer(mayo);
+}
+
+makeMeASandwich(false, false, false, true, false, false);
+
+if (sandwich.pickle === true) {
+  docOutput[docOutput.length] = "pickle";
+}
+if (sandwich.lettuce === true) {
+  docOutput[docOutput.length] = "lettuce";
+}
+if (sandwich.tomato === true) {
+  docOutput[docOutput.length] = "tomato";
+}
+if (sandwich.ketchup === true) {
+  docOutput[docOutput.length] = "ketchup";
+}
+if (sandwich.mustard === true) {
+  docOutput[docOutput.length] = "mustard";
+}
+if (sandwich.mayo === true) {
+  docOutput[docOutput.length] = "mayo";
+}
+if (docOutput.length === 0) {
+  docOutput[docOutput.length] = "nothing";
+}
+
+document.write("My sandwich has " + docOutput.join(", ") + " on it.");
